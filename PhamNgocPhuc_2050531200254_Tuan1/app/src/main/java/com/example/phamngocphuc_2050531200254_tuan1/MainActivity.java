e317debab1184530d9e7ef8df278823f533f79d1
PNP0972 <112404450+PNP0972@users.noreply.github.com>
2022-08-29 20:56:10 +0700

    commit

---
+++ PhamNgocPhuc_2050531200254_Tuan1/app/src/main/java/com/example/phamngocphuc_2050531200254_tuan1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.phamngocphuc_2050531200254_tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+}
