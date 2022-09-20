705cd5bd6b961342e3ab08c3d19fe8a316d1b35d
PNP0972 <112404450+PNP0972@users.noreply.github.com>
2022-09-20 22:37:26 +0700

    ListView

---
+++ ListView_PHUC/app/src/main/java/com/example/listview_phuc/profileActivity.java
@@ -0,0 +1,16 @@
+package com.example.listview_phuc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class profileActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+}
