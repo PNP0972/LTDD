1a9f6c0e42a7eb9f615869bfa6643fa0e29c6471
PNP0972 <112404450+PNP0972@users.noreply.github.com>
2022-10-21 18:48:46 +0700

    thi giua ki

---
+++ GK/app/src/main/java/caidat/dmt/gk/Profile.java
@@ -0,0 +1,32 @@
+package caidat.dmt.gk;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Profile extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_profile,container,false);
+        Button btn_logout = (Button) view.findViewById(R.id.btn_Logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+}
